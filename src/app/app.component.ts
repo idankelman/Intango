91691cbc898fd31b37170784d2f215793e51ac95
idankelman <kelmanidan@gmail.com>
2022-02-07 17:16:29 +0200

    added on click output from the Square to the main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  // Vote:number = 1;
+
   title = 'Client';
+  
+  UpVote(){
+    console.log("Vote up")
+    // this.Vote++;
+  }
+
 }
