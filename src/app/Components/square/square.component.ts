3a6925537549e000417117ac10d9b6239f6daa7b
idankelman <kelmanidan@gmail.com>
2022-02-07 14:29:26 +0200

    Basic Structure for the Square component

--- src/app/Components/square/square.component.ts
+++ src/app/Components/square/square.component.ts
@@ -1,13 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+
+//================================================================
+//                          imports :
+//================================================================
+
+
+import { Component, OnInit ,Input,Output} from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+
+
+
+  //================================================================
+  //                      the cusotom elem :
+  //================================================================
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.css']
 })
+
+
+  //================================================================
+  //                            Cycle :
+  //================================================================
+
 export class SquareComponent implements OnInit {
 
   constructor() { }
+
+
+  //================================================================
+  //                            Functions :
+  //================================================================
+
+  BtnClick(){
+    console.log("Square Clicked");
+  }
+
 
   ngOnInit(): void {
   }
