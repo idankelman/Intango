12e9483a7a9b7fd066371ec7aac8b9245e701132
idankelman <kelmanidan@gmail.com>
2022-02-07 17:16:53 +0200

    Added dynamic Change of the bar width and Vote Change on Square component

--- src/app/Components/square/square.component.ts
+++ src/app/Components/square/square.component.ts
@@ -6,6 +6,8 @@
 
 import { Component, OnInit ,Input,Output} from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { Square } from 'src/app/Interfaces/Square';
+import { NgForm } from '@angular/forms';
 
 
 
@@ -27,15 +29,53 @@ import { EventEmitter } from '@angular/core';
 
 export class SquareComponent implements OnInit {
 
-  constructor() { }
+  //================================================================
+  //                            variables :
+  //================================================================
+
+
+  @Input() Square_:Square 
+  @Output() btnClick= new EventEmitter();
+
+  SquareWidth:number = 200;
+  BarWidth:number = 50;
+  // BarWidth;
+  MaxWidth:number = 200;
+
+
+  constructor() {
+
+
+
+    //=====================    init variables   =====================
+    this.Square_ = {
+      id : -1,
+      color:"#fff",
+      votes: 0
+    };
+    // this.BarWidth= this.CalcBar(this.Square_.votes)
+
+   }
 
 
   //================================================================
   //                            Functions :
   //================================================================
 
-  BtnClick(){
+  onClick(){
     console.log("Square Clicked");
+    this.btnClick.emit();
+    this.Square_.votes+=1
+    this.BarWidth = this.CalcBar(this.Square_.votes) 
+    console.log(this.BarWidth)
+  }
+
+  CalcBar(Votes:number){
+    return  Math.floor(this.SquareWidth*(Votes/this.MaxWidth));
+  }
+
+  onChange(){
+
   }
 
 
