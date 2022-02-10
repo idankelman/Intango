51022e3fda0d341c1fdce757c316eac41bb7ca8e
idankelman <kelmanidan@gmail.com>
2022-02-10 17:38:49 +0200

    final touches for production

--- src/app/WebSocketAPI.ts
+++ src/app/WebSocketAPI.ts
@@ -19,9 +19,7 @@ export class WebSocketAPI {
         const _this = this;
         _this.stompClient.connect({}, function (frame:any) {
             _this.stompClient.subscribe(_this.topic, function (sdkEvent:any) {
-                //console.log(sdkEvent.body);
                 _this.onMessageReceived(sdkEvent);
-                // _this._send(-1);
             });
             //_this.stompClient.reconnect_delay = 2000;
         }, this.errorCallBack);
@@ -47,13 +45,9 @@ export class WebSocketAPI {
   * @param {*} message 
   */
     _send(message:any) {
-        // console.log("calling logout api via web socket");
-        // console.log("sending test message to server : "+JSON.stringify(message));
-        // this.stompClient.send("/app/hello", {}, JSON.stringify(message));
         this.stompClient.send("/app/hello", {}, message);
     }
     onMessageReceived(message:any) {
-        // console.log("Message Recieved from Server :: " + message);
         this.appComponent.handleMessage(message.body);
     }
 }
