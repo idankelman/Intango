3979936870ad4d4a32205a91fc020eadb8d3fed5
idankelman <kelmanidan@gmail.com>
2022-02-10 14:05:49 +0200

    added a title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SquareComponent } from './Components/square/square.component';
+import { TitleComponent } from './Components/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SquareComponent,
+    TitleComponent,
     
   ],
   imports: [
