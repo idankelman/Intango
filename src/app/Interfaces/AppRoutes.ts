06228d33eab1a76acef6e6f4061ea50a22c7928c
idankelman <kelmanidan@gmail.com>
2022-02-26 23:50:35 +0200

    added default redirected to main page

--- src/app/Interfaces/AppRoutes.ts
+++ src/app/Interfaces/AppRoutes.ts
@@ -1,8 +1,12 @@
 import { Routes } from "@angular/router"
 import { HomeComponent } from "../Pages/home/home.component"
-import { SquareComponent } from "../Components/square/square.component"
+import { FavoritesComponent } from "../Pages/favorites/favorites.component"
+import { AdminComponent } from "../Pages/admin/admin.component"
 
 export const AppRoutes:Routes= [
     {path:"Home" , component: HomeComponent},
-    {path:"Favorites" , component: SquareComponent}
+    {path:"Favorites" , component: FavoritesComponent},
+    {path:"Admin" , component: AdminComponent},
+    // {path:"*", redirectTo:"/Home" , pathMatch:"full"},
+    {path:"**", redirectTo:"/Home" , pathMatch:"full"}
 ]
