0808499adb07eef6df4191df0290d366c2b4a18d
idankelman <kelmanidan@gmail.com>
2022-02-26 23:09:31 +0200

    Included the required imports to make the routing possible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SquareComponent } from './Components/square/square.component';
 import { TitleComponent } from './Components/title/title.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { AppRoutes } from './Interfaces/AppRoutes';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,12 @@ import { HomeComponent } from './Pages/home/home.component';
     SquareComponent,
     TitleComponent,
     NavbarComponent,
-    HomeComponent,    
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
