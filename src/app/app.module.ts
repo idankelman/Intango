9c9c45e84beb125b0d371ef827d1b0093e7bcb82
idankelman <kelmanidan@gmail.com>
2022-02-26 22:20:15 +0200

    init create new component navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './Components/square/square.component';
 import { TitleComponent } from './Components/title/title.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SquareComponent,
     TitleComponent,
-    
+    NavbarComponent,    
   ],
   imports: [
     BrowserModule,
