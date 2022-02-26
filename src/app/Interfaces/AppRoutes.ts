12fd9194c31035faf3a295d17474708be772e15f
idankelman <kelmanidan@gmail.com>
2022-02-27 00:22:00 +0200

    Routing to selected element

--- src/app/Interfaces/AppRoutes.ts
+++ src/app/Interfaces/AppRoutes.ts
@@ -2,11 +2,12 @@ import { Routes } from "@angular/router"
 import { HomeComponent } from "../Pages/home/home.component"
 import { FavoritesComponent } from "../Pages/favorites/favorites.component"
 import { AdminComponent } from "../Pages/admin/admin.component"
+import { SquareComponent } from "../Components/square/square.component"
 
 export const AppRoutes:Routes= [
     {path:"Home" , component: HomeComponent},
     {path:"Favorites" , component: FavoritesComponent},
+    {path:"Square/:id" , component: SquareComponent},
     {path:"Admin" , component: AdminComponent},
-    // {path:"*", redirectTo:"/Home" , pathMatch:"full"},
     {path:"**", redirectTo:"/Home" , pathMatch:"full"}
 ]
