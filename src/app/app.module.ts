a584f950ef0aedb43aae14675763550b0dafa465
idankelman <kelmanidan@gmail.com>
2022-02-26 22:49:55 +0200

    Init Home Page Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SquareComponent } from './Components/square/square.component';
 import { TitleComponent } from './Components/title/title.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SquareComponent,
     TitleComponent,
-    NavbarComponent,    
+    NavbarComponent,
+    HomeComponent,    
   ],
   imports: [
     BrowserModule,
