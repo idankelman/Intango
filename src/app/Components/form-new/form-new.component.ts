b0795c047ba063fb25174f9e2140752f67780456
idankelman <kelmanidan@gmail.com>
2022-03-03 03:47:21 +0200

    chnaged input names , and dynamically bound the square component to the inputs

--- src/app/Components/form-new/form-new.component.ts
+++ src/app/Components/form-new/form-new.component.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Square } from 'src/app/Interfaces/Square';
 
 @Component({
   selector: 'app-form-new',
   templateUrl: './form-new.component.html',
-  styleUrls: ['./form-new.component.css']
+  styleUrls: ['./form-new.component.css'],
 })
 export class FormNewComponent implements OnInit {
-  name: string = "";
-  email: string = "";
-  message: string = "";
+  square: Square;
+  name: string = '';
+  Color: string = '';
+  Votes: number = 0;
 
-  constructor() { }
+  constructor() {
+    this.square = {
+      id: -1,
+      color: '#fff',
+      votes: 0,
+    };
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ChangeName() {
+    console.log('ChangeName');
+    let t = this.name;
+  }
+  ChangeColor() {
+    console.log('ChangeColor');
+    let tmp = this.Color;
+    if (tmp.length > 0 && tmp.charAt(0) === '#') {
+      while (tmp.length < 7) {
+        tmp += 'F';
+      }
+    }
+    this.square.color = tmp;
+    console.log(tmp);
+  }
+  ChangeVote() {
+    console.log('ChangeVote');
+    if (this.Votes >= 0) {
+      this.square.votes = this.Votes;
+    }
   }
 
   onSubmit(form: any) {
     console.log(form);
   }
-
 }
