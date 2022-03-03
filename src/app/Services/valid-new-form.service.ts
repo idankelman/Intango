747deb0e7f2c1ea7f6dde001ab2ac0102e55c5d9
idankelman <kelmanidan@gmail.com>
2022-03-03 15:52:10 +0200

    Validate method

--- src/app/Services/valid-new-form.service.ts
+++ src/app/Services/valid-new-form.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Square } from '../Interfaces/Square';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidNewFormService {
+  //================================================================
+  //                  variables
+  //================================================================
+
+  currentSquare!: Square; 
 
   constructor() { }
+
+
+
+  //================================================================
+  //                  Functions
+  //================================================================
+
+ Validate(vote:number ,color:string){
+  this.currentSquare.color=color;
+  this.currentSquare.votes=vote;
+  this.currentSquare.id=Math.floor(Math.random()*1000);
+ }
 }
