f6d37e8a520a59de7b45a0aa8696362afe698d75
idankelman <kelmanidan@gmail.com>
2022-03-06 15:06:48 +0200

    Creating Form Group Controls

--- src/app/Components/reactive-form/reactive-form.component.ts
+++ src/app/Components/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Square } from 'src/app/Interfaces/Square';
 import { ValidNewFormService } from 'src/app/Services/valid-new-form.service';
@@ -9,12 +10,23 @@ import { ValidNewFormService } from 'src/app/Services/valid-new-form.service';
   styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
+  
+
+  //variables
+  //================================================================
   square: Square;
   id: number = 0;
+  EditSquare!:FormGroup
+
+
+  //constructor
+  //================================================================
+
   constructor(
     private route: ActivatedRoute,
-    private SquareService: ValidNewFormService
-  ) {
+    private SquareService: ValidNewFormService)
+  
+  {
     this.square = {
       id: -1,
       color: '#fff',
@@ -23,12 +35,27 @@ export class ReactiveFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    //setting the right square to the form :
+    //=======================================
+    
     let id = this.route.snapshot.paramMap.get('id');
     console.log(id);
     if (id !== null) {
       console.log('the id :' + id);
       this.square = this.SquareService.getSquare(parseInt(id));
       console.log(this.square);
+    
     }
+        //creating new form controls : 
+    //=======================================
+
+    let Color = new FormControl()
+    let Votes = new FormControl()
+    this.EditSquare = new FormGroup({
+      Color: Color,
+      Votes: Votes
+    });
+
   }
 }
