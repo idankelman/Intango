7f210aaa3c80a345844cfde5ee86ec0f2213142d
idankelman <kelmanidan@gmail.com>
2022-03-06 14:35:55 +0200

    reactive form able to recognize components from the server

--- src/app/Components/reactive-form/reactive-form.component.ts
+++ src/app/Components/reactive-form/reactive-form.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Square } from 'src/app/Interfaces/Square';
+import { ValidNewFormService } from 'src/app/Services/valid-new-form.service';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  square: Square;
+  id: number = 0;
+  constructor(
+    private route: ActivatedRoute,
+    private SquareService: ValidNewFormService
+  ) {
+    this.square = {
+      id: -1,
+      color: '#fff',
+      votes: 0,
+    };
   }
 
+  ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    if (id !== null) {
+      console.log('the id :' + id);
+      this.square = this.SquareService.getSquare(parseInt(id));
+      console.log(this.square);
+    }
+  }
 }
