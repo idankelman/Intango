9480adeeff41f0de200c134ac61035abe4bd2af4
idankelman <kelmanidan@gmail.com>
2022-03-06 20:01:00 +0200

    creating the custom pipe functionality and using it inside the html

--- src/app/Pipes/custom-pipe.pipe.ts
+++ src/app/Pipes/custom-pipe.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomPipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number): string {
+    switch(value){
+      case 0: return "zero";
+      case 1: return "one";
+      case 2: return "two";
+      case 3: return "three";
+      case 4: return "four";
+      case 5: return "five";
+      case 6: return "six";
+    }
+    return value.toString();
   }
 
 }
