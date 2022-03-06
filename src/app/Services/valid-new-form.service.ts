599cfa202704568671551aa787aa30b65ab249c6
idankelman <kelmanidan@gmail.com>
2022-03-06 16:49:30 +0200

    fixed bug with color

--- src/app/Services/valid-new-form.service.ts
+++ src/app/Services/valid-new-form.service.ts
@@ -3,59 +3,63 @@ import { SquareComponent } from '../Components/square/square.component';
 import { Square } from '../Interfaces/Square';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ValidNewFormService {
   //================================================================
   //                  variables
   //================================================================
 
-  allSquares :Square[] = [];
-  newSquares: Square[]= []; 
+  allSquares: Square[] = [];
+  newSquares: Square[] = [];
   ChangeSquare!: Square;
 
-  constructor() { }
-
+  constructor() {}
 
   //================================================================
   //                  Functions
   //================================================================
 
-  updateSquares(Squares:Square[]){
+  updateSquares(Squares: Square[]) {
     this.allSquares = Squares;
   }
 
-  getSquare(id:number):Square{
+  getSquare(id: number): Square {
     // console.log("the given id is :"+id);
-    let Square = this.allSquares.find(square => square.id == id);
-    return Square==undefined?{id:-1,color:"#fff",votes:0}:Square;
+    let Square = this.allSquares.find((square) => square.id == id);
+    return Square == undefined ? { id: -1, color: '#fff', votes: 0 } : Square;
   }
 
- Validate(id:number,vote:number ,Color:string){
-   if(id<0)
-    id = Math.floor(Math.random()*1000)
-  let newSquare:Square ={
-    id: id,
-    color:Color.toUpperCase(),
-    votes:vote
-  }
-  this.newSquares.push(newSquare);  
- }
+  Validate(id: number, vote: number, Color: string) {
 
- Edit(id:number,vote:number ,Color:string)
- {
-    let index = this.allSquares.findIndex(square => square.id == id);
-    if(index<0)
-      return;
+    if (id < 0) id = Math.floor(Math.random() * 1000);
+    let tmp = Color;
+    if (tmp.length > 0 && tmp.charAt(0) === '#')
+      while (tmp.length < 7) 
+        tmp += 'F';
+
+    let newSquare: Square = {
+      id: id,
+      color: tmp.toUpperCase(),
+      votes: vote,
+    };
+
+    this.newSquares.push(newSquare);
     
+  }
+
+  Edit(id: number, vote: number, Color: string) {
+    let index = this.allSquares.findIndex((square) => square.id == id);
+    if (index < 0) return;
+
     this.ChangeSquare = {
       id: id,
-      color:Color.toUpperCase(),
-      votes:vote
-    }    
- }
+      color: Color.toUpperCase(),
+      votes: vote,
+    };
+  }
 
- Authenticate(){
-  return !!this.newSquares;
- }
+  Authenticate() {
+    return !!this.newSquares;
+  }
 }
