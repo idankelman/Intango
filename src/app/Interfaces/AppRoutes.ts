88e50639ae77d5e04b7384dd3c540e92ecec3a8f
idankelman <kelmanidan@gmail.com>
2022-03-06 14:35:01 +0200

    redirected to the new reactive form component

--- src/app/Interfaces/AppRoutes.ts
+++ src/app/Interfaces/AppRoutes.ts
@@ -4,11 +4,12 @@ import { FavoritesComponent } from "../Pages/favorites/favorites.component"
 import { AdminComponent } from "../Pages/admin/admin.component"
 import { SquareComponent } from "../Components/square/square.component"
 import { FormNewComponent } from "../Components/form-new/form-new.component"
+import { ReactiveFormComponent } from "../Components/reactive-form/reactive-form.component"
 
 export const AppRoutes:Routes= [
     {path:"Home" , component: HomeComponent},
     {path:"Favorites" , component: FavoritesComponent},
-    {path:"Square/:id" , component: SquareComponent},
+    {path:"Square/:id" , component: ReactiveFormComponent},
     {path:"Admin" , component: AdminComponent},
     {path:"newSquare" , component: FormNewComponent},
     {path:"**", redirectTo:"/Home" , pathMatch:"full"}
