c848c7eba6107abfe2a90e6fd8f88f706a23674d
idankelman <kelmanidan@gmail.com>
2022-03-07 14:58:20 +0200

    implemented the directive component property

--- src/app/Directives/modal-trigger.directive.ts
+++ src/app/Directives/modal-trigger.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Directive({
   selector: '[appModalTrigger]'
 })
-export class ModalTriggerDirective {
+export class ModalTriggerDirective implements OnInit{
 
-  constructor() { }
+  //----------------------------------------------------------------------------------
+  //                          Properties
+
+  elem! : HTMLElement;
+
+
+  //----------------------------------------------------------------------------------
+  //binding the element to the property of the element that is being passed in
+  constructor(ref : ElementRef) { 
+    this.elem = ref.nativeElement;
+  }
+
+
+  //----------------------------------------------------------------------------------
+  //                          Functions
+
+  
+  //----------------------------------------------------------------------------------
+  //                          adding event listener to the element
+  ngOnInit(): void {
+    this.elem.addEventListener('click',this.CloseModal);
+  }
+
+
+  //----------------------------------------------------------------------------------
+  //                          closing the modal
+  CloseModal() {
+    $(".Wrapper").hide();
+  }
 
 }
