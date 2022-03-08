63f76caa10a80edf7b0c13d61908c17e748e58b1
idankelman <kelmanidan@gmail.com>
2022-03-08 21:21:53 +0200

    simple unit test

---
+++ src/app/app.spec.ts
@@ -0,0 +1,20 @@
+
+
+describe('Test TEst',()=>{
+    it('should pass',()=>{
+        expect(true).toBe(true);
+    });
+});
+
+describe('first test',()=>{
+    it('should pass',()=>{
+        expect(true).toBe(true);
+    });
+});
+
+
+describe('Second', () => {
+    it('should pass', () => {
+        expect(!false).toBe(true);
+    });
+});
