517709315bdd92133e01bc15bf542a35d7ee650f
idankelman <kelmanidan@gmail.com>
2022-03-08 02:20:51 +0200

    added fucntionality to requester service .

--- src/app/Services/requests.service.ts
+++ src/app/Services/requests.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestsService {
 
-  constructor() { }
+  //--------------------------------------------------------------------------------
+  //                          Variables
+  url:string = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+
+  constructor(private Http: HttpClient) {}
+
+  //---------------------------------------------------------------------------------
+  //                          Functions
+
+  
+  getUrl(){
+    return this.url
+  }
+
+  //---------------------------------------------------------------------------------
+  //                          return observable of the request
+  getData(url= this.url): Observable<any> {
+    // return this.Http.get(url, {responseType:'json'}).pipe(catchError(this.errorHandler('getUrl',[])));
+    return this.Http.get(url, {responseType:'json'});
+  }
+
+
+  //---------------------------------------------------------------------------------
+  //                          error handling
+
+  private errorHandler<T> (operation = 'operation', result?: any) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      return of(result as T);
+    }
+  }
+
+
 }
