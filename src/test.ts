576fa0a4bee2c95f1aa867f2a1c6726cb1dc17f6
idankelman <kelmanidan@gmail.com>
2022-03-08 21:21:45 +0200

    created the test script that finds all of the .spec.ts files inside the app and executes them

---
+++ src/test.ts
@@ -0,0 +1,34 @@
+
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import 'zone.js/dist/zone-testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: any;
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+// Then we find all the tests.
+const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);
+
+// describe('first test',()=>{
+//     it('should pass',()=>{
+//         expect(true).toBe(true);
+//     });
+// });
+
+
+// describe('Second', () => {
+//     it('should pass', () => {
+//         expect(!false).toBe(true);
+//     });
+// });
