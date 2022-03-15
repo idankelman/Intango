0766cb67c4942165eca492f788d3d2145c6e1d95
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-03-15 11:02:08 +0200

    function and template that reads and executes toastrs

--- src/app/Pages/admin/admin.component.ts
+++ src/app/Pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastrService:ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  Toast(id:number){
+    if(id==1){
+      this.toastrService.success('Successfully Added', 'Success');
+    }
+    else if(id==2){
+      this.toastrService.error('Error in Adding', 'Error');
+    }
+    else if(id==3){
+      this.toastrService.info('Info', 'Info');
+    }
+    else if(id==4){
+      this.toastrService.warning('Warning', 'Warning');
+    }
+    
   }
 
 }
