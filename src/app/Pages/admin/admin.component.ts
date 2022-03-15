31bcb9008b54a0b863aed5c19d923acea2e1c581
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-03-15 10:41:05 +0200

    displaying the local storage time when logged in

--- src/app/Pages/admin/admin.component.ts
+++ src/app/Pages/admin/admin.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  time:String = "logged in at : ";
+
   constructor() { }
 
   ngOnInit(): void {
+    let info = localStorage.getItem('time');
+    if(info){
+      this.time += info;
+    }
+  }
+
+  Reset(){
+    // localStorage.clear();
+    // localStorage.removeItem('time');
+    
   }
 
 }
